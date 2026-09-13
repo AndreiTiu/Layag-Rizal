e06a50011ec0890f6b2f41160cd3b9bd5afc5bd9
Andrei Tiu <andreitiu49@gmail.com>
2026-09-13 15:42:20 +0800

    Initial Layag Rizal project

---
+++ src/main/java/com/rizalcourier/rizal_courier/RizalCourierApplication.java
@@ -0,0 +1,13 @@
+package com.rizalcourier.rizal_courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizalCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizalCourierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizalcourier/rizal_courier/RizalCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizalcourier.rizal_courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizalCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
